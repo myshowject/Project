7697f4c84e62331d3cd43f573427f5034e32aad5
myshowject <1033409122@qq.com>
2018-12-08 21:45:18 +0800

    [轮播图静态页面更新一部分]

--- pc端/ms/routes/index.js
+++ pc端/ms/routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const pool = require("../pool");
 
 // http://localhost:8000/index
-router.get("/",(req,res)=>{
-  var sql = "SELECT *FROM xz_index_product WHERE seq_re"
-  pool.query(sql,[],(err,result)=>{
-    if(err) throw err;
-    res.send(result)
-  })
-})
+// router.get("/",(req,res)=>{
+//   var sql = "SELECT *FROM xz_index_product WHERE seq_re"
+//   pool.query(sql,[],(err,result)=>{
+//     if(err) throw err;
+//     res.send(result)
+//   })
+// })
 
 module.exports = router;
